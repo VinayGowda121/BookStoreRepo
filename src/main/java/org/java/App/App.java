88934049c401638c174764381e78f0380020d70e
VinayS <vinay.sgsg.com>
2020-04-29 20:24:02 +0530

    dev branch commit

--- src/main/java/org/java/App/App.java
+++ src/main/java/org/java/App/App.java
@@ -13,6 +13,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 public class App {
 
 	public static void main(String[] args) {
+System.out.println("Git branch code");
 		SpringApplication.run(App.class, args);
 	}
 
